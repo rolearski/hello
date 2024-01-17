6f2f388fda0f63dc47ac6fdb2772ee6bed1c2dc9
Romek Olearski <romek@olearski.pl>
2024-01-17 04:44:29 +0100

    Cosmetic ...

--- main.cpp
+++ main.cpp
@@ -1,41 +1,26 @@
+
 #include <iostream>
 #include <sstream>
-#include <bitset>
-#include <algorithm>
-
-#include <iterator>
-#include <iostream>
-#include <sstream>
-
-
-#include <map>
-#include <vector>
-#include <set>
-#include <queue>
-#include <deque>
-#include <list>
-
-#include <thread>
-#include <mutex>
-
 
 using namespace std;
 
 #include "helloworld.h"
 
 #define GETTEXT_PACKAGE "rc"
-//#define PROGRAMNAME_LOCALEDIR "/usr/share/locale/"
+
+// OK #define PROGRAMNAME_LOCALEDIR "/usr/share/locale/"
 
 /// OK 
 #define PROGRAMNAME_LOCALEDIR "locale"
+
 // OK #define PROGRAMNAME_LOCALEDIR "/home/romek/.local/share/locale/"
+
 // NOK #define PROGRAMNAME_LOCALEDIR "~/.local/share/locale/"
 
 int main (int argc, char *argv[])
 {
 
   //cerr << "LC_ALL " << setlocale (LC_ALL, NULL) /*getenv("LC_ALL")*/  << ", LANG " << getenv("LANG") << ", GDM_LANG " << getenv("GDM_LANG") << ", LANGUAGE " << getenv("LANGUAGE") << endl;
-
   auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examplehello");
 
   setlocale (LC_ALL, "");
@@ -61,6 +46,5 @@ int main (int argc, char *argv[])
 
   HelloWorld helloworld("Hello World");
 
-  //Shows the window and returns when it is closed.
   return app->run(helloworld);
 }
